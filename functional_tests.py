06bf8a80d22508a9b23932b50ca8979be2081a6e
edipd <shzdev@gmail.com>
2015-04-12 17:46:11 -0400

    Basic vew now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -5,7 +5,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait()
+        self.browser.implicitly_wait(2)
     def tearDown(self):
         self.browser.quit()
     def test_can_start_a_list_and_retrive_it_later(self):
